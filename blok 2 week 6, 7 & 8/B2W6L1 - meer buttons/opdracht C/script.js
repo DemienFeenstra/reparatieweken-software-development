b2f179f30ed0a91d4322db4e65bc37b2aadefaec
DemiënFeenstra <99064788@mydavinci.nl>
2021-05-31 11:48:24 +0200

    opdracht: meer buttons afgerond

---
+++ blok 2 week 6, 7 & 8/B2W6L1 - meer buttons/opdracht C/script.js
@@ -0,0 +1,21 @@
+var color = ["red", "purple", "blue", "black", "white"];
+
+
+for(i = 1; i <= 30; i++){
+	let btn = document.createElement("button");
+	btn.innerText = i;
+	btn.style.backgroundColor = 'green';
+	document.body.appendChild(btn);
+	container.appendChild(btn);
+	btn.onclick = function(){
+	click(this,color);
+	}
+}
+
+function click(button,color){
+  	var color2 = color.indexOf(button.style.backgroundColor);
+    button.style.backgroundColor = color[color2+1]
+    if(button.style.backgroundColor == "white"){
+        button.remove();
+    }
+}
